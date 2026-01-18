f2f3a4413c276d89d24e4c82c823bbb0aae0736d
Jun909 <junsiang97@hotmail.com>
2026-01-18 22:10:38 +0100

    Add LLM integration

---
+++ llm_config.py
@@ -0,0 +1,24 @@
+from langchain_ollama import ChatOllama
+from langchain_deepseek import ChatDeepSeek
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LLM_PROVIDER = getenv("LLM_PROVIDER")
+
+if LLM_PROVIDER == "ollama":
+    llm = ChatOllama(
+        model="mistral:latest",
+        temperature=0.1,
+    )
+elif LLM_PROVIDER == "deepseek":
+    llm = ChatDeepSeek(
+    model="deepseek-chat",
+    temperature=0,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+    api_key=getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com"
+)
